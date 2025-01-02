450cd24b94f4d6b7a08e8757e3651bf59f09bdf5
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2025-01-02 07:11:04 +0400

    fix: bringing back the edge

--- apps/litarchive/src/app/honoRPCClient.ts
+++ apps/litarchive/src/app/honoRPCClient.ts
@@ -1,12 +1,12 @@
 import { AppTypes } from "@apps/backend/src";
 import { hc } from "hono/client";
 
-const client = hc<AppTypes>(process.env.BACKEND_URL!);
+const client = hc<AppTypes>(process.env.NEXT_PUBLIC_BACKEND_URL!);
 type Client = typeof client;
 
 const hcWithType = (...args: Parameters<typeof hc>): Client =>
   hc<AppTypes>(...args);
 
-const honoClient = hcWithType(process.env.BACKEND_URL!);
+const honoClient = hcWithType(process.env.NEXT_PUBLIC_BACKEND_URL!);
 
 export default honoClient;
