d098c8f43eab2b62398357f8e09ee539bcfc7174
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2025-02-22 05:00:42 +0400

    fix: fixed author's page loading

--- apps/litarchive/src/app/authors/[authorId]/@authorBooks/loading.tsx
+++ apps/litarchive/src/app/authors/[authorId]/@authorBooks/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex w-fit min-w-[250px] items-center justify-center">
+    <div className="flex h-min w-full items-center justify-center">
       <Loader2 className="animate-spin" size={40} />
     </div>
   );
--- apps/litarchive/src/app/authors/[authorId]/page.tsx
+++ apps/litarchive/src/app/authors/[authorId]/page.tsx
@@ -47,7 +47,7 @@ export default async function AuthorPage({
 
   return (
     <div className="top-20 flex w-fit grow-[1] flex-col gap-4 self-start lg:sticky lg:w-auto">
-      <div className="relative min-h-[350px] w-fit min-w-[250px] overflow-hidden border-transparent p-1 outline outline-1 lg:rounded-lg lg:border-2">
+      <div className="relative max-h-[350px] min-h-[350px] w-fit min-w-[250px] max-w-[250px] overflow-hidden border-transparent p-1 outline outline-1 lg:rounded-lg lg:border-2">
         <Image
           src={
             author?.imageUrl ||
