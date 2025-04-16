6c796b8f2c12a2304dd9c4b60e163cbe93c309a3
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2025-04-16 19:11:56 +0400

    fix: fixed articles loading page

--- apps/litarchive/src/app/articles/[articleSlug]/loading.tsx
+++ apps/litarchive/src/app/articles/[articleSlug]/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function ArticleLoading() {
   return (
-    <div className="absolute flex h-screen w-full items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <Loader2 className="animate-spin" size={40} />
     </div>
   );
