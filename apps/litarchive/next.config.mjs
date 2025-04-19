83aa40fa6efa942e0f5a6360d4d5a968ef894e99
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2025-04-19 14:46:05 +0400

    fix: next images remote patter for prod r2

--- apps/litarchive/next.config.mjs
+++ apps/litarchive/next.config.mjs
@@ -27,7 +27,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://pub-7f190ec6a9c04e3e9220da81bf6e3ec9.r2.dev",
+        hostname: "pub-7f190ec6a9c04e3e9220da81bf6e3ec9.r2.dev",
         port: "",
       },
       {
@@ -35,11 +35,11 @@ const nextConfig = {
         hostname: "1abnppqngbtuiv5y.public.blob.vercel-storage.com",
         port: "",
       },
-      // {
-      //   protocol: "https",
-      //   hostname: "pub-e94a2a2980874e3ab621fd44043e7eca.r2.dev",
-      //   port: "",
-      // },
+      {
+        protocol: "https",
+        hostname: "pub-e94a2a2980874e3ab621fd44043e7eca.r2.dev",
+        port: "",
+      },
     ],
   },
 };
