a6c02389e02f27c5170917d9af3527524c6287da
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2025-04-23 19:30:39 +0400

    fix: fixed import for prod build

--- apps/litarchive/src/app/honoRPCClient.ts
+++ apps/litarchive/src/app/honoRPCClient.ts
@@ -1,4 +1,5 @@
-import { hcWithType } from "@apps/backend/dist/hc";
+import { hcWithType } from "@apps/backend/src/hc";
+// import { hcWithType } from "@apps/backend/dist/hc";
 
 const honoClient = hcWithType(process.env.NEXT_PUBLIC_BACKEND_URL!);
 
